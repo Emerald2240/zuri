30856fcc1cf66b6786448b8ba3403952253667f1
Orji Micheal <orjimichael4886@gmail.com>
2022-10-11 20:42:37 +0100

    Added Commenting

--- w3/user_data.php
+++ w3/user_data.php
@@ -1,6 +1,9 @@
 <?php
 echo '<h1>USER DATA PAGE</h1>';
+
+// Checks if post global array is empty
 if (!empty($_POST)) {
+    // Checks if userdata.csv already exists. If it doesnt exist, the first row of the csv file is created.
     if (!file_exists('userdata.csv')) {
         $fp = fopen('userdata.csv', 'a');
         fwrite($fp, 'Name,Email,Date_Of_Birth,Gender,Country,' . "\n");
@@ -9,11 +12,14 @@ if (!empty($_POST)) {
         $fp = fopen('userdata.csv', 'a');
     }
 
+    //All the different data is written to the file in the order of the title above. \n text is added to create a new line
+    #region
     fwrite($fp, '"' . $_POST['name'] . '",');
     fwrite($fp, $_POST['email'] . ',');
     fwrite($fp, $_POST['dateOfBirth'] . ',');
     fwrite($fp, $_POST['gender'] . ',');
     fwrite($fp, $_POST['country'] . ',' . "\n");
+    #endregion
 
     fclose($fp);
     echo 'File Updated Succesfully<br>';
