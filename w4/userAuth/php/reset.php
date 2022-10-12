2adcc146ebaedb4657f6fd74a5be00a1d2691a68
Orji Micheal <orjimichael4886@gmail.com>
2022-10-12 22:04:44 +0100

    Implemented Reset password functionality

--- w4/userAuth/php/reset.php
+++ w4/userAuth/php/reset.php
@@ -1,16 +1,68 @@
 <?php
-if(isset($_POST['submit'])){
-    $email = //complete this;
-    $newpassword = //complete this;
+if (isset($_POST['submit'])) {
+    $email = $_POST['email'];
+    $newpassword = $_POST['newpassword'];
 
-    resetPassword($email, $password);
+    if (resetPassword($email, $newpassword)) {
+        echo 'User succesfully updated';
+        // gotoPage('../forms/login.html');
+        echo ('<br><a href="../forms/login.html">Go to Login Page</a>');
+    } else {
+        echo 'User does not exist';
+        //gotoPage('../forms/resetpassword.html');
+        echo ('<br><a href="../forms/login.html">Go to Login Page</a>');
+    }
 }
 
-function resetPassword($email, $password){
+function resetPassword($email, $newpassword)
+{
     //open file and check if the username exist inside
     //if it does, replace the password
 
+    $changes = false;
+    $filename = '../storage/users.csv';
+    $data = [];
+
+    // open the file
+    $f = fopen($filename, 'r');
+
+    $data = [];
+
+    if ($f === false) {
+        die('Cannot open the file ' . $filename);
+    }
+
+    // read each line in CSV file at a time
+    while (($row = fgetcsv($f)) !== false) {
+        // print_r($row);
+        if ($row[1] == $email) {
+            $row[2] = $newpassword;
+            $changes = true;
+        }
+        $data[] = $row;
+    }
+
+    //print_r($data);
+
+    if ($changes == false) {
+        return false;
+    } else {
+        $fwrite = fopen($filename, 'w');
+        foreach ($data as $row) {
+            fputcsv($fwrite, $row);
+        }
+        fclose($fwrite);
+        return true;
+    }
+
+    // close the file
+    fclose($f);
+    return false;
 }
-echo "HANDLE THIS PAGE";
 
-
+function gotoPage($location)
+{
+    header('location:' . $location);
+    exit();
+}
+// echo "HANDLE THIS PAGE";
